94cce17f35e5ba9031dce90c8703cbc1abdc5c47
ceciliabarudi <blasterkat101@gmail.com>
2018-11-24 17:24:50 +0100

    add-test navbar partial

--- app.js
+++ app.js
@@ -1,46 +1,47 @@
-'use strict';
+'use strict'
 
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const hbs = require('hbs');
-const logger = require('morgan');
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const hbs = require('hbs')
+const logger = require('morgan')
 
-const indexRouter = require('./routes/index');
+const indexRouter = require('./routes/index')
 // const usersRouter = require('./routes/users');
 
-const app = express();
+const app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+hbs.registerPartials(path.join(__dirname, '/views/partials'))
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter);
+app.use('/', indexRouter)
 
 // -- 404 and error handler
 
 // NOTE: requires a views/not-found.ejs template
 app.use((req, res, next) => {
-  res.status(404);
-  res.render('not-found', { title: 'Page not found! | Playvine ' });
-});
+  res.status(404)
+  res.render('not-found', { title: 'Page not found! | Playvine ' })
+})
 
 // NOTE: requires a views/error.ejs template
 app.use((err, req, res, next) => {
   // always log the error
-  console.error('ERROR', req.method, req.path, err);
+  console.error('ERROR', req.method, req.path, err)
 
   // only render if the error ocurred before sending the response
   if (!res.headersSent) {
-    res.status(500);
-    res.render('error', { title: 'Server Error, sorry | Playvine ' });
+    res.status(500)
+    res.render('error', { title: 'Server Error, sorry | Playvine ' })
   }
-});
+})
 
-module.exports = app;
+module.exports = app
