77871be3b304d6773723ac35c8742be26374b662
Robby van Rheenen <robbyvanrheenen@gmail.com>
2018-11-25 23:22:14 +0100

    add server side validation and error messages

--- app.js
+++ app.js
@@ -1,30 +1,31 @@
 'use strict'
 
 // set up Express
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const hbs = require('hbs');
-const logger = require('morgan');
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const hbs = require('hbs')
+const logger = require('morgan')
+
+// set up flash error messaging
+const session = require('express-session')
+const flash = require('connect-flash')
 
 // set up the routes
-const indexRouter = require('./routes/index');
-const signUpRouter = require('./routes/signup');
+const indexRouter = require('./routes/index')
+const signUpRouter = require('./routes/signup')
 
 // start express
 const app = express()
 
 // set up connection to mongo database
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://localhost/playvine', {
   keepAlive: true,
   useNewUrlParser: true,
   reconnectTries: Number.MAX_VALUE
-});
-
-// set up flash error messaging
-const flash = require('connect-flash');
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
@@ -36,28 +37,33 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
+// use flash
+app.use(session({ cookie: { maxAge: 60000 },
+  secret: 'woot',
+  resave: false,
+  saveUninitialized: false }))
+app.use(flash())
+
 // default route handlers
-app.use('/', indexRouter);
-app.use('/signup', signUpRouter);
+app.use('/', indexRouter)
+app.use('/signup', signUpRouter)
 
 // 404 client error handler
 app.use((req, res, next) => {
-  res.status(404);
-  res.render('not-found', { title: 'Page not found! | Playvine ' });
-});
+  res.status(404)
+  res.render('not-found', { title: 'Page not found! | Playvine ' })
+})
 
 // 500 server error handler
 app.use((err, req, res, next) => {
   // always log the error
-  console.error('ERROR', req.method, req.path, err);
+  console.error('ERROR', req.method, req.path, err)
 
   // only render if the error ocurred before sending the response
   if (!res.headersSent) {
-    res.status(500);
-    res.render('error', { title: 'Server Error, sorry | Playvine ' });
+    res.status(500)
+    res.render('error', { title: 'Server Error, sorry | Playvine ' })
   }
 })
 
-app.use(flash());
-
-module.exports = app;
+module.exports = app
--- routes/signup.js
+++ routes/signup.js
@@ -1,40 +1,47 @@
-'use strict';
+'use strict'
 
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-const User = require('../models/user.js');
+const User = require('../models/user.js')
 
 /* GET signup page. */
 router.get('/', (req, res, next) => {
-  res.render('signup', { title: 'Playvine - Sign Up' });
-});
+  const data = {
+    messages: req.flash('mongoError'),
+    title: 'Playvine - Sign Up',
+    username: req.query.username
+  }
+  res.render('signup', data)
+})
 
 /* POST signup */
 router.post('/', (req, res, next) => {
   const newUser = {
     username: req.body.username,
     password: req.body.password
-  };
+  }
 
-  // first check if username already exists
-  User.find({username: newUser.username})
-    .then(() => {
-      res.send('This username already exists.');
-    })
-    .catch((error) => {
-      next(error);
-    });
+  // password validation
+  if (newUser.password.length < 8) {
+    req.flash('mongoError', 'Password should be at least 8 characters')
+    return res.redirect('/signup?username=' + encodeURIComponent(newUser.username))
+  }
 
   // insert user to database
-  User.create(newUser)
-    .then(() => {
-      res.redirect('/');
-    })
-    .catch((error) => {
-      console.log(error.code);
-      next(error);
-  });
-});
+  User.create(newUser, (err, user) => {
+    if (err) {
+      if (err.code === 11000) {
+        req.flash('mongoError', `Username '${newUser.username}' has already been taken.`)
+      } else {
+        throw err
+      }
+      return res.redirect('/signup?username=' + encodeURIComponent(newUser.username))
+    } else {
+      req.flash('userCreated', 'Your are ready to go!')
+      return res.redirect('/')
+    }
+  })
+})
 
-module.exports = router;
+module.exports = router
