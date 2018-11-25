e5006795d888c916a721bbe91a919fc5d327bb93
ceciliabarudi <blasterkat101@gmail.com>
2018-11-25 20:03:45 +0100

    finish/clean up homepage and navbar styles

--- routes/signup.js
+++ routes/signup.js
@@ -1,40 +1,40 @@
-'use strict';
+'use strict'
 
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-const User = require('../models/user.js');
+const User = require('../models/user.js')
 
 /* GET signup page. */
 router.get('/', (req, res, next) => {
-  res.render('signup', { title: 'Playvine - Sign Up' });
-});
+  res.render('signup', { title: 'Playvine - Sign Up' })
+})
 
 /* POST signup */
 router.post('/', (req, res, next) => {
   const newUser = {
     username: req.body.username,
     password: req.body.password
-  };
+  }
 
   // first check if username already exists
-  User.find({username: newUser.username})
+  User.find({ username: newUser.username })
     .then(() => {
-      res.send('This username already exists.');
+      res.send('This username already exists.')
     })
     .catch((error) => {
-      next(error);
-    });
+      next(error)
+    })
 
   // insert user to database
   User.create(newUser)
     .then(() => {
-      res.redirect('/');
+      res.redirect('/')
     })
     .catch((error) => {
-      console.log(error.code);
-      next(error);
-  });
-});
+      console.log(error.code)
+      next(error)
+    })
+})
 
-module.exports = router;
+module.exports = router
