362682fc287cb05dc3d83dab8d8ebe1b68d50ccc
ceciliabarudi <blasterkat101@gmail.com>
2018-11-27 17:05:35 +0100

    put the get route for creation back in sessionsjs, fix navbar top to 0

--- routes/sessions.js
+++ routes/sessions.js
@@ -5,6 +5,27 @@ const router = express.Router();
 const MusicSession = require('../models/musicSession');
 const checkCurrentUser = require('../middleware/checkCurrentUser.js');
 
+/* GET session list page */
+router.get('/', (req, res, next) => {
+  // create middleware for !req.session.currentUser
+  if (req.session.currentUser) {
+    MusicSession.find({}).sort({ location: 1, startTime: 1 })
+      .then((musicSessions) => {
+        // musicSessions.startTime = Moment(musicSessions.startTime).format('DD/MM/YYYY');
+        console.log('musicSessions: ' + musicSessions);
+        return res.render('sessions', { title: 'Playvine | Sessions', musicSessions });
+      })
+      .catch(next);
+  } else {
+    return res.redirect('/');
+  }
+});
+
+router.get('/create', (req, res, next) => {
+  // login validation
+  res.render('sessions/create', { title: 'Playvine | Create a session ' });
+});
+
 router.post('/', (req, res, next) => {
   // login validation
   // create a new session, then make a for loop to
@@ -29,22 +50,6 @@ router.post('/', (req, res, next) => {
       the action= field in the hbs form */
     })
     .catch(next);
-});
-
-/* GET session list page */
-router.get('/', (req, res, next) => {
-  // create middleware for !req.session.currentUser
-  if (req.session.currentUser) {
-    MusicSession.find({}).sort({ location: 1, startTime: 1 })
-      .then((musicSessions) => {
-        // musicSessions.startTime = Moment(musicSessions.startTime).format('DD/MM/YYYY');
-        console.log('musicSessions: ' + musicSessions);
-        return res.render('sessions', { title: 'Playvine | Sessions', musicSessions });
-      })
-      .catch(next);
-  } else {
-    return res.redirect('/');
-  }
 });
 
 module.exports = router;
